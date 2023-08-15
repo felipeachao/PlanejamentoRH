bde53b0583afddd7ffb8bddb76854c0de4d306aa
Felipe Achao <felipe.achao@gmail.com>
2023-08-15 08:30:52 -0400

    feat: add employee model

--- src/employee/model/employee.model.ts
+++ src/employee/model/employee.model.ts
@@ -1,1 +1,17 @@
-export class Employee {}
+import { Model } from 'sequelize';
+import { Column, Table } from 'sequelize-typescript';
+
+@Table({
+  tableName: 'Employee',
+  timestamps: true,
+})
+export class EmployeeModel extends Model<EmployeeModel> {
+  @Column({ primaryKey: true, autoIncrement: true })
+  id: number;
+
+  @Column
+  name: string;
+
+  @Column
+  registration: number;
+}
