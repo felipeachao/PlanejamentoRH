846e81e0a27fdbede453b17becb07f83ac9cca0c
Felipe Achao <felipe.achao@gmail.com>
2023-08-15 10:15:35 -0400

    feat: add sequelize import

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { EmployeeController } from './employee.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { EmployeeModel } from './model/employee.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([EmployeeModel])],
   controllers: [EmployeeController],
-  providers: [EmployeeService]
+  providers: [EmployeeService],
 })
 export class EmployeeModule {}
